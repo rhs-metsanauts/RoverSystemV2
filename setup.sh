7f1a939a4cbbc57b355eb96441c52e20281252eb
rover <rover@localhost>
2026-04-02 11:46:33 -0500

    change setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 sudo apt update
 sudo apt install python3-gpiozero
 sudo apt upgrade
+sudo apt install python3-pip
 sudo pip3 install sparkfun-qwiic
-pip install gpiozero rpigpio --break-system-packages
-pip install pyyaml --break-system-packages
-pip install pyzed --break-system-packages
+sudo pip3 install gpiozero
+sudo pip3 install pyyaml
+sudo pip3 install pyzed
