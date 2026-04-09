903a8f368bf9b1119140f08ae96d563e03fed970
skandacode <39654761+skandacode@users.noreply.github.com>
2026-04-09 17:54:47 -0500

    change motors to use pwm

---
+++ Rover.py
@@ -0,0 +1,51 @@
+from RockerBogie import RockerBogie
+from Drivetrain import Drivetrain
+import time
+
+class Rover:
+    def __init__(self):
+        self.rocker_bogie = RockerBogie()
+        self.drivetrain = Drivetrain()
+        self.toRegularPosition()
+        self.stop()
+    def toSunPosition(self):
+        self.rocker_bogie.toSunPosition()
+    def toRegularPosition(self):
+        self.rocker_bogie.toRegularPosition()
+    def setPowers(self, left_power, right_power):
+        self.drivetrain.setPowers(left_power, right_power)
+    def forward(self, power):
+        self.setPowers(power, power)
+    def backward(self, power):
+        self.setPowers(-power, -power) 
+    def turnLeft(self, power):
+        self.setPowers(-power, power)
+    def turnRight(self, power):
+        self.setPowers(power, -power)
+    def stop(self):
+        self.setPowers(0, 0)
+
+    def forwardForDuration(self, power, duration):
+        self.forward(power)
+        time.sleep(duration)
+        self.stop()
+    def backwardForDuration(self, power, duration):
+        self.backward(power)
+        time.sleep(duration)
+        self.stop()
+    def turnLeftForDuration(self, power, duration):
+        self.turnLeft(power)
+        time.sleep(duration)
+        self.stop()
+    def turnRightForDuration(self, power, duration):
+        self.turnRight(power)
+        time.sleep(duration)
+        self.stop()
+
+if __name__ == "__main__":
+    import time
+    rover = Rover()
+    rover.forwardForDuration(1, 2)
+    rover.turnLeftForDuration(1, 1)
+    rover.turnRightForDuration(1, 1)
+    rover.stop()
