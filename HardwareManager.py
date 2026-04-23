f6685bdfa349dc3ff6f04d1cb65f46f2c5b12d13
rover <rover@localhost>
2026-04-22 20:27:49 -0500

    remove untested files

--- HardwareManager.py
+++
@@ -1,4 +0,0 @@
-#uses unix sockets to control all parts of the rover from a single interface
-#uses a config file to determine which hardware components are present and how to control them
-from Config import get_config
-
